954a1fdf032622dd27090ffdc10e00f92875105d
GrUnD <teneschevgrund@gmail.com>
2025-10-05 01:24:30 +0700

    [DEV] Base

---
+++ MegabonkMultiplayer/MegabonkMultiplayerBootstrap.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Il2CppInterop.Runtime.Injection;
+using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace MegabonkMultiplayer
+{
+    [BepInPlugin("com.yourname.megabonk.multiplayer", "Megabonk Multiplayer", "1.0.0")]
+    public class MegabonkMultiplayerBootstrap : BasePlugin
+    {
+        internal static new ManualLogSource Log;
+
+        private static GameObject MultiplayerMenuObject;
+
+        public override void Load()
+        {
+            Log = base.Log;
+
+            Log.LogInfo("Megabonk Multiplayer Mod loaded!");
+            ClassInjector.RegisterTypeInIl2Cpp<MultiplayerMenu>();
+
+            MultiplayerMenuObject = new GameObject("MegabonkMultiplayer");
+            MultiplayerMenuObject.AddComponent<MultiplayerMenu>();
+            GameObject.DontDestroyOnLoad(MultiplayerMenuObject);
+        }
+
+        public override bool Unload()
+        {
+            if (MultiplayerMenuObject != null)
+            {
+                Object.Destroy(MultiplayerMenuObject);
+                MultiplayerMenuObject = null;
+            }
+
+            Log.LogInfo("Plugin Megabonk Multiplayer unloaded!");
+            return true;
+        }
+
+    }
+}
---
+++ MegabonkMultiplayer/MultiplayerMenu.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx.Unity.IL2CPP.UnityEngine;
+using Il2CppSystem;
+using System;
+using UnityEngine;
+
+
+namespace MegabonkMultiplayer
+{
+    public class MultiplayerMenu : MonoBehaviour
+    {
+        private bool showMenu = false;
+        private string ipAddress = "127.0.0.1";
+
+        public void Awake()
+        {
+            this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+            MegabonkMultiplayerBootstrap.Log.LogInfo("Awake called");
+        }
+
+        public void Start()
+        { 
+            MegabonkMultiplayerBootstrap.Log.LogInfo("Start called");
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyInt(BepInEx.Unity.IL2CPP.UnityEngine.KeyCode.H))
+            {
+                Debug.Log("H Pressed");
+            }
+
+            if (Input.GetKeyInt(BepInEx.Unity.IL2CPP.UnityEngine.KeyCode.M))
+            {
+                Debug.Log("M Pressed");
+                showMenu = !showMenu;
+            }
+
+        }
+    }
+}
